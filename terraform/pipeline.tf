e407884030bd6460af6aaafd56e3ef86d03b116f
Glenn Bech <glenn.bech@gmail.com>
2018-10-14 13:34:43 +0200

    pipeline

--- terraform/pipeline.tf
+++ terraform/pipeline.tf
@@ -4,14 +4,32 @@ resource "heroku_app" "ci" {
   region = "eu"
 }
 
+# Create a database, and configure the app to use it
+resource "heroku_addon" "db_ci" {
+  app  = "${heroku_app.ci.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
 resource "heroku_app" "staging" {
   name   = "${var.app_prefix}-app-staging"
   region = "eu"
 }
 
+# Create a database, and configure the app to use it
+resource "heroku_addon" "db_stage" {
+  app  = "${heroku_app.staging.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
 resource "heroku_app" "production" {
   name   = "${var.app_prefix}-app-production"
   region = "eu"
+}
+
+# Create a database, and configure the app to use it
+resource "heroku_addon" "db_prod" {
+  app  = "${heroku_app.production.name}"
+  plan = "heroku-postgresql:hobby-dev"
 }
 
 resource "heroku_pipeline" "test-app" {
